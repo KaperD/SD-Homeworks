89714009d34b63067ee68a52dc800414ddd0dd15
Andrey Shein <andrey.shein.spb@gmail.com>
2022-02-06 23:13:49 +0300

    fix wc command tests

--- bash-cli/src/test/kotlin/ru/cli/commands/WcCommandTest.kt
+++ bash-cli/src/test/kotlin/ru/cli/commands/WcCommandTest.kt
@@ -2,9 +2,9 @@ package ru.cli.commands
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.io.File
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
+import kotlin.io.path.*
 
 class WcCommandTest {
     private fun calculate(files: List<String>): String {
@@ -25,22 +25,25 @@ class WcCommandTest {
 
     @Test
     fun emptyFileTest() {
-        val expected = File("src/test/resources/wc-empty-file.out").readText()
-        val tested = calculate(listOf("src/test/resources/empty-file.txt"))
+        val expected = Path("src", "test", "resources", "wc-empty-file.out").readText()
+        val tested = calculate(listOf(Path("src", "test", "resources", "empty-file.txt").pathString))
         Assertions.assertEquals(expected, tested)
     }
 
     @Test
     fun loremIpsumTest() {
-        val expected = File("src/test/resources/wc-lorem-ipsum.out").readText()
-        val tested = calculate(listOf("src/test/resources/lorem-ipsum.txt"))
+        val expected = Path("src", "test", "resources", "wc-lorem-ipsum.out").readText()
+        val tested = calculate(listOf(Path("src", "test", "resources", "lorem-ipsum.txt").pathString))
         Assertions.assertEquals(expected, tested)
     }
 
     @Test
     fun twoFilesTest() {
-        val expected = File("src/test/resources/wc-two-files.out").readText()
-        val tested = calculate(listOf("src/test/resources/lorem-ipsum.txt", "src/test/resources/forest-gump.txt"))
+        val expected = Path("src", "test", "resources", "wc-two-files.out").readText()
+        val tested = calculate(listOf(
+            Path("src", "test", "resources", "lorem-ipsum.txt").pathString,
+            Path("src", "test", "resources", "forest-gump.txt").pathString
+        ))
         Assertions.assertEquals(expected, tested)
     }
 }
