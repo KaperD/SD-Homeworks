30721960f8bb06bdfe860ad42aa35f7bc6f429cc
Danil Bubnov <dkbubnov@edu.hse.ru>
2022-02-23 14:43:48 +0300

    Add isDirectory check to Environment

--- bash-cli/src/main/kotlin/ru/cli/Environment.kt
+++ bash-cli/src/main/kotlin/ru/cli/Environment.kt
@@ -6,10 +6,16 @@ import java.io.File
  * This class stores our environmental variables in hash map
  * and our current working directory path
  */
-data class Environment(
+class Environment(
     val vars: MutableMap<String, String> = HashMap(),
-    var workingDir: File = File("").absoluteFile
+    workingDir: File = File("").absoluteFile
 ) {
+    var workingDir: File = workingDir
+        set(value) {
+            assert(value.isDirectory)
+            field = value
+        }
+
     init {
         assert(workingDir.isDirectory)
     }
